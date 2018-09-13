9fdc08e6a7633c56735447f4178784d862050114
Febby315 <564822672@qq.com>
2018-09-13 15:03:12 +0800

    修改逻辑 方便后期扩展

---
+++ ripples/config/config.js
@@ -0,0 +1,16 @@
+
+// 随机壁纸接口
+// 孟坤博客壁纸接口
+var mkblog = "https://cdn.mom1.cn/?mom=302";
+// Bing每日壁纸接口
+var bing = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10";
+// 360壁纸接口
+var _360CHROME = {
+    tag: "http://cdn.apc.360.cn/index.php?c=WallPaper&a=getAllCategoriesV2&from=360chrome",
+    new: "http://wallpaper.apc.360.cn/index.php?c=WallPaper&a=getAppsByOrder&order=create_time&start=${start}&count=${count}&from=360chrome",
+    column: "http://wallpaper.apc.360.cn/index.php?c=WallPaper&a=getAppsByCategory&cid=${cid}&start=${start}&count=${count}&from=360chrome"
+};
+// 姬长信API
+var isoyu = "https://api.isoyu.com";
+// 小歪二次元图片api接口
+var xwboke = "https://www.xwboke.cn/api/api.php";
--- ripples/js/script.js
+++ ripples/js/script.js
@@ -1,62 +1,62 @@
+// image转Blob
+function imageToBlob(src, cb){
+    var img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext('2d');
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        canvas.toBlob(function(blob){ cb(URL.createObjectURL(blob)); });
+    }
+    img.src = src;
+}
+
 //创建右键菜单组件
-var epMenu={
-    create:function(point,option){
-        var $menu = $("#epMenu");
-        if(!$menu.length){
-            //没有菜单节点的时候创建一个
-            $menu = $("<div></div>").attr('id','epMenu').attr('class','epMenu').css({left:point.left+'px',top:point.top+'px'});
-            $("body").append($menu);
-        }else $menu.html('');//清空里面的内容
-        
-        for(var x in option){
-            var $ta=$("<a></a>").text(option[x]['name']).on('click',function(){
-                this.action?this.action():null;
-                $(".epMenu").remove();
-            }.bind(option[x]));
-            $menu.append($ta);
+var epMenu = {
+    create: function(e,items){
+        var $menu = $("#epMenu").css({ left: e.clientX+'px', top: e.clientY+'px' }).html(null).show();
+        for(var i=0,len=items.length; i<len; i++){
+            var $item = $("<a></a>").text(items[i].name).on('click',function(){
+                typeof(this.action)=="function" ? this.action() : null;
+                $menu.hide();
+            }.bind(items[i]));
+            $menu.append($item);
         }
     },
-    destory:function(){
-        $(".epMenu").remove();
-    }    
+    destory: function(){
+        $("#epMenu").hide().remove();
+    }
 };
+
+//获取分类
 
 // 程序入口
 $(document).ready(function() {
-    function getObjectURL(file) {
-        return window.createObjcectURL?window.createObjcectURL(file):
-            window.URL&&window.URL.createObjectURL?window.URL.createObjectURL(file):
-                window.webkitURL&&window.webkitURL.createObjectURL?window.webkitURL.createObjectURL(file):
-                    null;
-    }
-    var rippleEle = $('#ripple').ripples({
-        arbitrary:10,
-        resolution: 850,
-        dropRadius: 10,
-        perturbance: 0.02
-    })[0];
-    $(rippleEle).bind("mousedown", function(e){
-        e.which==2?epMenu.create({left:e.clientX,top:e.clientY},[
-            {name:'更换壁纸','action':function(){ $("#file").trigger("click"); }},
-            {name:'暂停/播放','action':function(){ $("#pause").trigger("click"); }},
-            {name:'隐藏/显示','action':function(){ $("#show").trigger("click"); }}
-        ]):null;
+    var rippleEle = $('#ripple').ripples({ arbitrary:10, resolution: 850, dropRadius: 10, perturbance: 0.02, crossOrigin: 'anonymous' })[0];
+    var items = [
+        {name:'更换壁纸','action':function(){ $("#file").trigger("click"); }},
+        {name:'暂停/播放','action':function(){ $("#pause").trigger("click"); }},
+        {name:'隐藏/显示','action':function(){ $("#show").trigger("click"); }}
+    ];
+    $(rippleEle).on("mousedown", function(e){
+        e.which==2 ? epMenu.create(e, items) : null;
         return false;
     }).on("click contextmenu",function(){
-        $(".epMenu").remove();
+        $("#epMenu").hide();
     });
     $("#file").on("change",function(){
-        var fileurl=getObjectURL(this.files[0]);
-        $(rippleEle).css({'background-image':'url('+fileurl+')'});
-        $(rippleEle).ripples("set","imageUrl",fileurl);
+        var src = URL.createObjectURL(this.files[0]);
+        $(rippleEle).ripples("set","imageUrl",src).css("background-image", template.render("url({{src}})", { src: src }) );
     });
     $("#show").on("click",function(){
         $(this).val(rippleEle.isHide?"隐藏":"显示");
-        $(rippleEle).prop("isHide",!rippleEle.isHide).ripples(rippleEle.isHide?"hide":'show');
+        $(rippleEle).prop("isHide",!rippleEle.isHide).ripples(rippleEle.isHide?"hide":"show");
     });
     $("#pause").on("click",function(){
         $(this).val(rippleEle.isStop?"暂停":"播放");
-        $(rippleEle).prop("isStop",!rippleEle.isStop).ripples(rippleEle.isStop?"pause":'play');
+        $(rippleEle).prop("isStop",!rippleEle.isStop).ripples(rippleEle.isStop?"pause":"play");
     });
     $("#destroy").on("click",function(){
         $("#pause,#show,#destroy").remove();
