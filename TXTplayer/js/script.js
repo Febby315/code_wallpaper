0d5bd334536211efd304e6cb70dd6734a4cdeb05
Febby315 <564822672@qq.com>
2018-09-30 16:51:09 +0800

    修改字符画逻辑

--- TXTplayer/js/script.js
+++ TXTplayer/js/script.js
@@ -24,7 +24,7 @@ var v_app = new Vue({
 // 配置灰度字符映射表
 var charMap = getCharsMap();
 function getCharsMap() {
-    var map = {}, chars = ['@', '#', '$', 'w', 'k', 'd', 't', 'i', ':', '.', '&nbsp;'];
+    var map = {}, chars = ['@', '$', 'w', 'k', 'd', 't', 'i', ':', '·', '&nbsp;'];
     var step = ~~(256/(chars.length-1));  //映射步长=最大字符长度/映射字符长度
     for (var i = 0; i < 256; i++) {
         map[i] = chars[~~(i / step)];
@@ -32,27 +32,31 @@ function getCharsMap() {
     return map;
 }
 // 图像转字符画
-function toChars(context, width, height, rowChars) {
+function toChars(ctx, width, height, rowChars) {
+    var fontSize = 7||~~($("#app").css("font-size").replace("px","")), lineHeight = 15||~~($("#app").css("line-height").replace("px",""));// 获取容器字体大小&行高
     var videoScale = width/height;//判断视频方向 >1宽屏 =1方屏 <1窄屏
-    var screenScale = $(document).width()/$(document).height();//判断屏幕方向 >1宽屏 =1方屏 <1窄屏
-    // var zoomScale = screenScale>videoScale ? height/$(document).height() : width/$(document).width(); //获取视频填充缩放比=屏幕宽高比>视频宽高比?高度缩放比:挎包都缩放比
-    var fontSize = ~~($("#app").css("font-size").replace("px","")), lineHeight = ~~($("#app").css("line-height").replace("px",""));// 获取容器字体大小&行高
-    
-    var frame = "",imageData = context.getImageData(0, 0, width, height),
-        cols = screenScale>videoScale ? $(document).height()/15 : $(document).width()/7, //字符画宽度=宽屏?按高度自适应:按宽度自适应;
+    var screenScale = $(window).width()/$(window).height();//判断屏幕方向 >1宽屏 =1方屏 <1窄屏
+    // var zoomScale = screenScale>videoScale ? height/$(window).height()/lineHeight : width/$(window).width()/fontSize; //获取视频填充缩放比=屏幕宽高比>视频宽高比?高度缩放比:挎包都缩放比
+    // var cs = ~~(width*zoomScale), rs = ~~(height*zoomScale) // 计算字符画宽高
+    // var cw = width/cs, ch = height/rs;
+    // console.log($(window).width(),$(window).height(),$(window).width(),$(window).height());
+    // console.log("视频宽高比:%s\n屏幕宽高比:%s\n视频缩放比:%s\n字符画宽度:%s\n字符画高度:%s\n灰度采集块宽度:%s\n灰度采集块高度:%s\n容器字体大小:%s\n容器字体行号:%s",videoScale, screenScale, zoomScale, cs, rs, cw, ch,fontSize,lineHeight);
+    // var cols=cs,rows=rs,char_w=cw,char_h=ch;
+    var frame = "",image = ctx.getImageData(0, 0, width, height);
+        cols = screenScale>videoScale ? $(window).height()/lineHeight : $(window).width()/fontSize, //字符画宽度=宽屏?按高度自适应:按宽度自适应;
         // cols = width < rowChars ? width : rowChars, // 获取列数=视频宽度<默认字符画宽度:视频宽度:默认字符画宽度 //(如果视频宽度小于默认字符画宽度则以视频宽度为准))
-        // cols = $(document).width()/7;
+        // cols = $(window).width()/7;
         char_w = char_h = width / cols, // 获取字符画缩放比=视频宽度/字符画宽度 //按视频宽度获取字符画的缩放比例 (宽高缩放比保持一致(防止变形))
         rows = height / char_h; // 获取行数
     // 计算一个像素区域的平均灰度值
     var getBlockGray = function (x, y, w, h) {
-        var sumGray = 0, pixels = w * h, data = imageData.data;
+        var sumGray = 0, pixels = w * h, data = image.data;
         for (var row = 0; row < w; row++) {
             for (var col = 0; col < h; col++) {
                 var cx = x + col, cy = y + row, // //current position x,current positon y
-                    index = (cy * imageData.width + cx) * 4, //current index in rgba data array
-                    R = data[index], G = data[index + 1], B = data[index + 2],
-                    gray = ~~(R * 0.3 + G * 0.6 + B * 0.1); //类似于PS中的RGB通道根据权重计算灰度
+                    index = (cy * image.width + cx) * 4, //current index in rgba data array
+                    R = data[index]||128, G = data[index + 1]||128, B = data[index + 2]||128,
+                    gray = ~~(0.5*R + 0.3*G + 0.2*B); //类似于PS中的RGB通道根据权重计算灰度
                 sumGray += gray;
             }
         }
@@ -64,7 +68,7 @@ function toChars(context, width, height, rowChars) {
             var avg = getBlockGray( ~~(c * char_h), ~~(r * char_h) , ~~char_w, ~~char_h);   //获取灰度均值
             frame += charMap[avg];
         }
-        frame += '<br/>';
+        frame += '<br/>\n';
     };
     return frame;
 }
@@ -74,8 +78,13 @@ var captureImage = function () {
     if (canvas.width) {
         var ctx = canvas.getContext('2d');  //获取画布
         ctx.clearRect(0, 0, canvas.width, canvas.height);   //清除画布
-        ctx.drawImage($video[0], 0, 0, canvas.width, canvas.height);   //将视频当前画面绘制至画布
+        ctx.drawImage($video[0], 0, 0, canvas.width, canvas.height);   //将视频当前帧画面绘制至画布
         v_app.content = toChars(ctx, canvas.width, canvas.height, document.body.clientWidth/12*2);  //将画布图像转换为字符画
+        //输出到console
+        v_app.$nextTick(function () {
+            console.clear();
+            console.error("%c%s","font-family: Courier New; font-size: 12px;line-height:8px;",$("#app").text());
+        });
     }
 }
 // 开始位置
@@ -94,5 +103,6 @@ $video.on("play",function(){
     }, 20);
 });
 $video.on("pause ended",function(){
+    // 视频暂停或结束停止定时器
     clearInterval(window.timer);
 });
--- player/js/music.js
+++ player/js/music.js
@@ -1,3 +1,20 @@
-function play(){
-    var sound = new Howl({ src:["music/gbqq.flac"], autoplay: true, loop: true, volume: 0.5 });
-};
+var v_app = new Vue({
+    el: "#app",
+    data: {
+        sound:new Howl({
+            src: ["music/gbqq.flac"],
+            buffer: true,
+            autoplay: true,
+            loop: true,
+            volume: 0.5,
+            rate:1.5
+        })
+    },
+    mounted:function(){
+        this.$nextTick(function(){
+            console.log("sound",this.sound);
+        });
+    },
+    methods: {
+    }
+});
