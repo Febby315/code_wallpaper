0533e07dcef6d5002abea03b5e7c903295a80360
Febby315 <564822672@qq.com>
2018-12-28 18:14:10 +0800

    优化播放 新增 播放缓存(无声播放)

--- TXTplayer/js/script.js
+++ TXTplayer/js/script.js
@@ -1,3 +1,11 @@
+window.RAF = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 17);
+    };
 $(function(){
     // 利用vue虚拟DOM技术加速DOM节点数据渲染
     var v_app =window.v_app= new Vue({
@@ -7,27 +15,32 @@ $(function(){
             fileInput: document.getElementById("file"),
             video: document.getElementById("video"),
             canvas: document.createElement("canvas"),
+            stats: new Stats(),
+            cacheFrame:[],
+            cache: [],
+            cacheLenght: 5, currCacheIndex: 0,
             fontSize: 7||~~($("#view").css("font-size").replace("px","")), 
             lineHeight: 8||~~($("#view").css("line-height").replace("px","")),// 获取容器字体大小&行高
             chars: ['&nbsp;', '·', ':', 'i', 't', 'd', 'k', 'w', '$', '@'],
             sw: $(window).width(), sh: $(window).height(),// 获取初始屏幕宽高
-            vw: 0, vh: 0,
-            hasColor: true,    //是否输出色彩
+            vw: 0, vh: 0,// 储存视频宽高
+            cols: 0,rows: 0, char_w: 0, char_h: 0,// 字符画行列及采集块宽高
+            hasColor: true,// 是否输出色彩
             spanTempFn : doT.template('<span style="color:rgb({{=it.R}},{{=it.G}},{{=it.B}});">{{=it.T}}</span>')   //字符画像素模板
         },
+        // 动态计算
         computed:{
             // 配置灰度字符映射表
             charMap:function () {
-                let map = [], step = ~~(256/(this.chars.length-1));  // 映射步长=最大字符长度/映射字符长度
+                let map = [], step = ~~(256/(this.chars.length-1));// 映射步长=最大字符长度/映射字符长度
                 for (let i = 0; i < 256; i++) {
                     map[i] = this.chars[~~(i / step)];
                 };
                 return map;
             },
-            // 判断视频、屏幕方向(宽高比) >1宽屏 =1方屏 <1窄屏
             // 视频宽高比
             videoScale: function () {
-                return this.vw / this.vh;
+                return this.vw / this.vh;// 判断视频、屏幕方向(宽高比) >1宽屏 =1方屏 <1窄屏
             },
             // 屏幕宽高比
             screenScale:function(){
@@ -44,51 +57,87 @@ $(function(){
         },
         mounted: function () {
             // 初始化结束后// 开始位置
-            this.$nextTick(function () {
-                _this = this;
+            this.$nextTick(this.init);
+        },
+        methods: {
+            // 初始化
+            init(){
+                this.stats.domElement.style.position = 'absolute';
+                this.stats.domElement.style.left = '0px';
+                this.stats.domElement.style.top = '0px';
+                document.body.appendChild( this.stats.domElement );
+                for(let i=this.cache.length; i<this.cacheLenght; i++){
+                    this.cache.push(document.createElement("canvas"));
+                }
+                // $("#cache").append(...this.cache);
+                this.initEvent();
+            },
+            // 初始化事件
+            initEvent(){
+                let _this = this;
                 // 窗口大小改变
-                $(window).on("resize",function(){
+                $(window).on("resize",function(e){
                     _this.sw = $(window).width();
                     _this.sh = $(window).height();// 获取屏幕宽高
+                    _this.resetToCharsConfig();
                 });
                 // 文件更改时修改视频源
-                $(_this.fileInput).on("change",function(){
+                $(_this.fileInput).on("change",function(e){
                     window.timer ? clearInterval(window.timer) : null;
                     $(_this.video).attr("src",URL.createObjectURL(this.files[0]));
+                    _this.resetToCharsConfig();
                 });
                 // 视频源meta加载后
-                $(_this.video).on("loadedmetadata",function(){
-                    let vw =_this.vw= _this.video.videoWidth, vh =_this.vh= _this.video.videoHeight;
-                    _this.canvas.width = vw/_this.sw>0.2?_this.sw/5:vw;// 更新画布大小
+                $(_this.video).on("loadedmetadata",function(e){
+                    let vw =_this.vw= this.videoWidth, vh =_this.vh= this.videoHeight;
+                    _this.canvas.width = vw/_this.sw>0.2 ? _this.sw/5 : vw;// 更新画布大小
                     _this.canvas.height = vh*_this.canvas.width/vw;
+                    for(let i=0, len=_this.cache.length, cw=_this.canvas.width, ch=_this.canvas.height; i<len; i++){
+                        _this.cache[i].width = cw;
+                        _this.cache[i].height = ch;
+                    }
+                    _this.resetToCharsConfig();
                 });
                 // 开始播放
-                $(_this.video).on("play",function(){
+                $(_this.video).on("play",function(e){
                     $("#tool").hide();  // 隐藏工具栏
-                    setTimeout(function(){
-                        window.timer = setInterval(function (){
-                            _this.captureImage(_this.canvas.getContext('2d'));// 定时捕获画布画面并绘制字符画
-                        }, 34);// 1000/34=29.41fps
-                    },1000);
+                    setTimeout(_this.play,0);
                 });
                 // 暂停/结束
                 $(_this.video).on("pause ended",function(e){
                     e.type=="ended" ? _this.content=null : null;
                     clearInterval(window.timer);// 视频暂停或结束停止定时器
-                    setTimeout(function (){
-                        $("#tool").show();// 1秒后还原工具栏
-                    },1000);
+                    setTimeout(function(){
+                        $("#tool").show();
+                    },1000);// 1秒后还原工具栏
                 });
-            });
-        },
-        methods: {
+            },
+            // 更新画面
+            update(frame){
+                this.cacheFrame.push(frame);
+                this.content = frame;
+                this.stats.update();
+            },
+            // 重置采集参数
+            resetToCharsConfig(){
+                this.cacheFrame = [];
+                let t_rows, t_cols, cw=this.canvas.width, ch=this.canvas.height;// 字符画行数(高度)、字符画列数(宽度)、灰度采集块宽度、灰度采集块高度
+                // 根据视频、屏幕宽高比比值决定通过高度自适应或宽度自适应获得字符画宽度或高度
+                this.screenScale>this.videoScale ? t_rows=this.maxCols>ch?ch:this.maxCols : t_cols=this.maxRows>cw?cw:this.maxRows
+                this.char_w=this.char_h=t_rows ? ch/t_rows : cw/t_cols;// 灰度采集块宽高 = 视频高度/字符画高度||视频宽度/字符画宽度
+                t_rows ? t_cols=cw/this.char_w : t_rows=ch/this.char_h;// 字符画高度存在时计算字符画宽度否则计算字符画高度
+                this.cols = t_cols, this.rows = t_rows;// 临时数据储存
+            },
             // 计算一个像素区域的平均灰度值和灰度映射字符及RGB值对象
             getAvgGray(offset_x, offset_y, w, h,cw,ch, imgDate) {
                 let pixels=w*h, R=0, G=0, B=0;// 总像素数,默认RGB色
                 let sumGray=0, sumR=0, sumG=0, sumB=0;// 初始化区域总灰度,
+                let py,px;
                 for (let i=0; i < h; i++) {
+                    py = offset_y+i;
                     for (let j=0; j < w; j++){
-                        let py = offset_y+i, px = offset_x+j, idx = (py*cw+px)*4;// 当前像素坐标位置(原点:左上角)及起始指针位置(每像素由rgba数据组成)
+                        px = offset_x+j, 
+                        idx = (py*cw+px)*4;// 当前像素坐标位置(原点:左上角)及起始指针位置(每像素由rgba数据组成)
                         R = imgDate[idx]||R, G = imgDate[idx+1]||G, B = imgDate[idx+2]||B;// 获取当前像素RGB值,无效值则取上次记录值或0
                         sumGray+=(R*30 + G*59 + B*11 + 50); sumR+=R; sumG+=G; sumB+=B;// 类似于PS中的RGB通道根据权重计算灰度
                     }
@@ -96,40 +145,59 @@ $(function(){
                 let avgGray = ~~((sumGray/100)/pixels);// 获取区域平均灰度及平均RGB色彩值 为提高效率将单像素灰度计算中的除以100提出
                 return {
                     Gray: avgGray,
-                    T: _this.charMap[avgGray],
+                    T: this.charMap[avgGray],
                     R: ~~(sumR/pixels),
                     G: ~~(sumG/pixels),
                     B: ~~(sumB/pixels)
                 };
             },
             // 图像转字符画
-            toChars(ctx, cw, ch) {
-                let _this = this;
-                let spanTempFn = _this.spanTempFn, hasColor = _this.hasColor ,imgDate = ctx.getImageData(0, 0, cw, ch).data;// 字符画帧数据,当前画布图像数据
-                let rows, cols, char_w, char_h;// 字符画行数(高度)、字符画列数(宽度)、灰度采集块宽度、灰度采集块高度
-                // 根据视频、屏幕宽高比比值决定通过高度自适应或宽度自适应获得字符画宽度或高度
-                _this.screenScale>_this.videoScale ? rows=_this.maxCols>ch?ch:_this.maxCols : cols=_this.maxRows>cw?cw:_this.maxRows
-                char_w=char_h=rows ? ch/rows : cw/cols;// 灰度采集块宽高 = 视频高度/字符画高度||视频宽度/字符画宽度
-                rows ? cols=cw/char_w : rows=ch/char_h;// 字符画高度存在时计算字符画宽度否则计算字符画高度
+            toChars(ctx, cw, ch, callback) {
+                let imgDate = ctx.getImageData(0, 0, cw, ch).data, hasColor = this.hasColor, spanTempFn = this.spanTempFn;// 当前画布图像数据,是否包含配色,字符画模板
+                let cols_len = this.cols, rows_len = this.rows ,char_w = this.char_w, char_h = this.char_h;
                 // 遍历每个字符画像素获取灰度值映射字符追加至字符画帧数据
                 let frameRows = [];
-                for (let r = 0; r < rows; r++) {
+                for (let r=0; r<rows_len; r++) {
                     let frameCols = [];
-                    for (let c = 0; c < cols; c++) {
-                        let op = _this.getAvgGray(~~(char_w*c), ~~(char_h*r) , ~~(char_w), ~~(char_h), cw, ch, imgDate);// 获取灰度均值
+                    for (let c=0; c<cols_len; c++) {
+                        let op = this.getAvgGray(~~(char_w*c), ~~(char_h*r) , ~~(char_w), ~~(char_h), cw, ch, imgDate);// 获取灰度均值
                         frameCols.push(hasColor ? spanTempFn(op) : op.T);// 获取灰度映射字符追加入帧(仅灰度):获取灰度映射字符追加入帧(含色彩,较消耗性能)
                     }
                     frameRows.push(frameCols.join(""));
                 };
-                return frameRows.join("<br/>\n");
+                callback(frameRows.join("<br/>\n"));
             },
-            // 捕获视频当前帧画面并转化为字符画帧数据
-            captureImage (ctx) {
-                // TODO canvas 双缓冲减少卡顿
-                let cw = this.canvas.width, ch = this.canvas.height;
-                ctx.clearRect(0, 0, cw, ch); // 清除画布
-                ctx.drawImage(this.video, 0, 0, cw, ch);// 将视频当前帧画面绘制至画布
-                this.content = this.toChars(ctx, cw, ch);// 将画布图像数据转换为字符画
+            // 播放视频
+            play(){
+                var _this = this;
+                let ctx = this.canvas.getContext('2d'), cw = this.canvas.width, ch = this.canvas.height;
+                window.timer = setInterval(function (){
+                    // TODO canvas 双缓冲减少卡顿
+                    ctx.clearRect(0, 0, cw, ch); // 清除画布
+                    ctx.drawImage(this.video, 0, 0, cw, ch);// 将视频当前帧画面绘制至画布
+                    this.toChars(ctx, cw, ch, this.update);// 将画布图像数据转换为字符画
+                }.bind(this), 17);// 1000/34=29.41fps
+                // let loop = function loop(e){
+                //     if (!_this.video.paused) {
+                //         _this.currCacheIndex>=_this.cacheLenght ? _this.currCacheIndex=0 : null;
+                //         let currCacheCanvas = _this.cache[_this.currCacheIndex];
+                //         let ctx = currCacheCanvas.getContext('2d');
+                //         currCacheCanvas.getContext('2d').drawImage(_this.video, 0, 0, currCacheCanvas.width, currCacheCanvas.height);// 将视频当前帧画面绘制至画布 
+                //         _this.toChars(ctx, cw, ch, _this.update);// 将画布图像数据转换为字符画
+                //         _this.currCacheIndex++;
+                //         window.RAF(loop);
+                //     }
+                // }
+                // window.RAF(loop);
+            },
+            // 播放缓存
+            playCacheFrame(){
+                let cacheFrame = this.cacheFrame, playCacheFrameIndex = 0, len = cacheFrame.length;
+                window.timer = setInterval(function (){
+                    this.content = cacheFrame[playCacheFrameIndex];
+                    this.stats.update();
+                    playCacheFrameIndex>len ? clearInterval(window.timer) : playCacheFrameIndex++;
+                }.bind(this), 17);// 1000/34=29.41fps
             }
         }
     });
